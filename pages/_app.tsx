ed8d187c8f54c2918be3383f4f2994e3718de736
devphaseX <wilsonace87@gmail.com>
2021-12-27 00:37:18 +0100

    This is that one commit, you just push out there but does nothing.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  if (!['/', ''].includes(router.pathname)) {
-    return router.push('/');
-  }
   return (
     <div className="app">
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,5 +21,5 @@ export default function Home() {
 }
 
 export function getStaticProps() {
-  return {};
+  return { props: {} };
 }
