f0278f8fcf5aaf168680ec9a1041c6f8535564e2
devphasex <wilsonace87@gmail.com>
2022-02-08 16:11:54 +0100

    edited some content

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -15,13 +15,13 @@ const About: FC = () => {
       <p className="info">
         Hey!, It&lsquo;s Ayomide, a frontend developer, dev advocate fully based
         in Lagos, Nigeria. I&lsquo;m passionate about building enrich web
-        experience, a core member of the oou(Olabisi Onabanjo state University)
-        developer&lsquo;s club. Having equiped with in dept frontend knowledge,
-        I&lsquo;m able to build in house tools which has helped in my ability to
-        create highly scable web product.During my free, I also tech people how
-        to code at school. Over the years, I&lsquo;ve built a number of Web
-        application using technology like Node js, Next js, Typescript, Mongodb,
-        Graphql.
+        experience, I led the OOU(Olabisi Onabanjo state University)
+        developer&lsquo;s club as a core member. Having equiped with in dept
+        frontend knowledge, I&lsquo;m able to build in house tools which has
+        helped in my ability to create highly scable web product.During my free,
+        I also tech people how to code at school. Over the years, I&lsquo;ve
+        built a number of Web application using technology like Node js,React
+        js, Next js, Typescript, Mongodb, Graphql.
       </p>
       <div className="about-contact">
         <Button href="">Download Resume</Button>
