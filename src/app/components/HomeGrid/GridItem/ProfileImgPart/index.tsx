94bd2ade1d9449a7fdec3f49e8e905127c67e084
Ayomide <ayomidelawal700@gmail.com>
2023-04-16 07:39:12 +0100

    build the profile image part

--- src/app/components/HomeGrid/GridItem/ProfileImgPart/index.tsx
+++ src/app/components/HomeGrid/GridItem/ProfileImgPart/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const ProfileImgPart: React.FC = () => {
-  return <div className="profile-img-part"></div>;
+  return (
+    <div className="profile-img-part">
+      <div>
+        <img src="/images/placeholder.png" alt="Ayomide photo" />
+      </div>
+    </div>
+  );
 };
 
 export { ProfileImgPart };
