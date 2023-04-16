fdd2d31f094550326275bbd8fef045b0f9e66270
Ayomide <ayomidelawal700@gmail.com>
2023-04-16 14:30:31 +0100

    build the about part

--- src/app/components/HomeGrid/GridItem/AboutPart/index.tsx
+++ src/app/components/HomeGrid/GridItem/AboutPart/index.tsx
@@ -1,7 +1,28 @@
+import { Header } from '@/app/components/UI/Header';
 import React from 'react';
 
 const AboutPart: React.FC = () => {
-  return <div className="about-part"></div>;
+  return (
+    <div className="about-part">
+      <div className="about-part-content">
+        <div>
+          <Header text="About" />
+        </div>
+        <div className="about-detail">
+          <p className="sm-1-rg">
+            My name is Lawal Ayomide. A creative developer with 2+ years of
+            experience, I’m currently based in Lagos, Nigeria. I’m a computer
+            engineering student in my final year of my bachealor degree
+            programHey, I am Ayomide, a creative developer with 2+ years of
+            experience, I’m currently based in Lagos, Nigeria. I’m a computer
+            engineering student in my final year of my bachealor degree programa
+            computer engineering student in my final year of my bachealor degree
+            programa computer e
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { AboutPart };
