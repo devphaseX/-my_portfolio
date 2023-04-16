0c13cb70b215582699588f8ee1f663c8318a40f0
Ayomide <ayomidelawal700@gmail.com>
2023-04-16 07:39:34 +0100

    build the profile name part

--- src/app/components/HomeGrid/GridItem/ProfileNamePart/index.tsx
+++ src/app/components/HomeGrid/GridItem/ProfileNamePart/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const ProfileNamePart: React.FC = () => {
-  return <div className="profile-name-part"></div>;
+  return (
+    <div className="profile-name-part">
+      <div className="flex center profile-name sm-2-md">
+        <p className="profile-name__label">Folio</p>
+        <h3 className="profile-name__content">Ayomide Lawal</h3>
+      </div>
+    </div>
+  );
 };
 
 export { ProfileNamePart };
